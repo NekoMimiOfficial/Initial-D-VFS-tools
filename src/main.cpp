2567e16bf3490f4fb3c950c096d32c4847172a4b
NekoMimiOfficial <nekomimi@tilde.team>
2025-01-21 00:48:23 +0300

    removing windows for now

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,6 @@
 
 int main (int argc, char *argv[])
 {
-  FileBuffer winTest("C:/Windows/System32/cmd.exe");
-  if (winTest.gets() > 0) {
-    #define __WIN32
-  }
   mainCLI(argc-1, argv);
   return 0;
 }
