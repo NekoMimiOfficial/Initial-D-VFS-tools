834109974c9ed438210a68e7c9976eafd06bf26c
NekoMimiOfficial <nekomimi@tilde.team>
2025-01-21 11:13:16 +0300

    autocomplete issue

--- win_script.py
+++ win_script.py
@@ -4,4 +4,4 @@ import sys
 file= sys.argv[1]+os.path.sep+"src"+os.path.sep+"vfs.cpp"
 win_file= sys.argv[1]+os.path.sep+"src"+os.path.sep+"win_vfs.cpp"
 os.remove(file)
-os.rename(win_file, win_file)
+os.rename(win_file, file)
