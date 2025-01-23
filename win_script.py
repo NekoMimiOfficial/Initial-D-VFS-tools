61f11f270063e0bfc3e7e32ef0c0c9cafddfee56
NekoMimiOfficial <nekomimi@tilde.team>
2025-01-23 10:47:05 +0300

    remove unnecessary files

--- win_script.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-file= sys.argv[1]+os.path.sep+"src"+os.path.sep+"vfs.cpp"
-win_file= sys.argv[1]+os.path.sep+"src"+os.path.sep+"win_vfs.cpp"
-os.remove(file)
-os.rename(win_file, file)
